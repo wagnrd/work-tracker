bd7fe3303b55877c9aa89331d6d2973b3526e491
wagnrd <projects@wagnrd.de>
2023-06-17 14:11:04 +0200

    add working example of menu

--- src/welcome_view.rs
+++ src/welcome_view.rs
@@ -1,4 +1,24 @@
-use relm4::{adw, gtk, ComponentParts, ComponentSender, SimpleComponent};
+use relm4::actions::{ActionGroupName, ActionName, RelmAction, RelmActionGroup};
+use relm4::adw::prelude::*;
+use relm4::gtk::gio;
+use relm4::gtk::prelude::*;
+use relm4::gtk::InputPurpose::Name;
+use relm4::{adw, gtk, ComponentParts, ComponentSender, RelmWidgetExt, SimpleComponent, WidgetRef};
+
+struct TestActionGroupName;
+
+impl ActionGroupName for TestActionGroupName {
+    const NAME: &'static str = "testgroup";
+}
+
+struct TestActionName;
+
+impl ActionName for TestActionName {
+    type Group = TestActionGroupName;
+    type Target = ();
+    type State = ();
+    const NAME: &'static str = "test";
+}
 
 pub struct WelcomeView;
 
@@ -28,6 +48,19 @@ impl SimpleComponent for WelcomeView {
         let start_button = adw::SplitButton::builder().label("Start work").build();
 
         root.set_center_widget(Some(&start_button));
+
+        let mut action_group = RelmActionGroup::<TestActionGroupName>::new();
+        let action = RelmAction::<TestActionName>::new_stateless(|_| {
+            println!("It worked!!");
+        });
+        action_group.add_action(action);
+        action_group.register_for_main_application();
+
+        let menu_item = RelmAction::<TestActionName>::to_menu_item("Test");
+        menu_item.set_detailed_action("app.test");
+        let menu = gio::Menu::default();
+        menu.append_item(&menu_item);
+        start_button.set_menu_model(Some(&menu));
 
         let model = WelcomeView;
         let widgets = WelcomeViewWidgets;
