3799e7251e3646584d833dcdcf270540518e6673
Denis Wagner <denis.wagner@check24.de>
2023-07-05 17:23:06 +0200

    add start time indicator

--- src/screens/welcome_screen/component.rs
+++ src/screens/welcome_screen/component.rs
@@ -4,6 +4,7 @@ use crate::components::time_picker_dialog::{
 use crate::screens::welcome_screen::actions::{
     CustomTimeActionName, NowActionName, StartButtonActionGroupName,
 };
+use chrono::Timelike;
 use relm4::actions::{RelmAction, RelmActionGroup};
 use relm4::adw::glib::clone;
 use relm4::adw::prelude::*;
@@ -28,6 +29,7 @@ pub enum WelcomeScreenInput {
 
 pub struct WelcomeScreenWidgets {
     dialog: Controller<TimePickerDialog>,
+    start_time_label: gtk::Label,
 }
 
 impl SimpleComponent for WelcomeScreen {
@@ -53,12 +55,20 @@ impl SimpleComponent for WelcomeScreen {
     ) -> ComponentParts<Self> {
         let vbox = gtk::Box::builder()
             .orientation(gtk::Orientation::Vertical)
+            .spacing(10)
             .build();
         root.set_center_widget(Some(&vbox));
 
         let start_button = adw::SplitButton::builder().label("Start work").build();
         vbox.append(&start_button);
 
+        let start_time_label = gtk::Label::builder()
+            .label("From now")
+            .halign(gtk::Align::Center)
+            .build();
+        vbox.append(&start_time_label);
+
+        // Button menu
         let custom_time_menu_item = RelmAction::<CustomTimeActionName>::to_menu_item("Custom time");
         let now_menu_item = RelmAction::<NowActionName>::to_menu_item("Now");
         let menu = gio::Menu::default();
@@ -95,7 +105,10 @@ impl SimpleComponent for WelcomeScreen {
             is_time_picker_dialog_visible: false,
             custom_start_time: None,
         };
-        let widgets = WelcomeScreenWidgets { dialog };
+        let widgets = WelcomeScreenWidgets {
+            dialog,
+            start_time_label,
+        };
 
         ComponentParts { model, widgets }
     }
@@ -118,6 +131,19 @@ impl SimpleComponent for WelcomeScreen {
             widgets.dialog.widget().set_visible(true);
         } else {
             widgets.dialog.widget().set_visible(false);
+        }
+
+        if let Some(custom_start_time) = self.custom_start_time {
+            widgets.start_time_label.set_label(
+                format!(
+                    "From {}:{}",
+                    custom_start_time.hour(),
+                    custom_start_time.minute()
+                )
+                .as_str(),
+            );
+        } else {
+            widgets.start_time_label.set_label("From now");
         }
     }
 }
